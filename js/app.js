a34032ec7689ecc3ce70f3ef7bc404daa5de24ae
bbouley <b.bouley@hotmail.co.uk>
2015-07-27 11:18:55 -0600

    added menu to select different tip amounts

--- js/app.js
+++ js/app.js
@@ -4,11 +4,30 @@ var button = document.getElementsByClassName('btn')[0];
 
 var output = document.getElementsByClassName('output')[0];
 
+var option = document.getElementsByClassName('select')[0];
+
 function tip20(number){
   var tip = (number/100)*20;
   return tip;
 }
 
+function tip15(number){
+  var tip = (number/100)*15;
+  return tip;
+}
+
+function tip10(number){
+  var tip = (number/100)*10;
+  return tip;
+}
+
+
+
 button.addEventListener('click', function(){
-  output.innerHTML = 'You should tip $' + tip20(input.value).toFixed(2);
-});
+  if (option[0].selected === true){
+  output.innerHTML = 'You should tip $' + tip10(input.value).toFixed(2);} else if(option[1].selected === true){
+  output.innerHTML = 'You should tip $' + tip15(input.value).toFixed(2);} else if(option[2].selected === true){
+  output.innerHTML = 'You should tip $' + tip20(input.value).toFixed(2)
+  }
+  }
+);
