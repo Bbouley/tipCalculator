7f1bc1639aad83c22d2a1bab49278fdaf984641f
bbouley <b.bouley@hotmail.co.uk>
2015-07-27 10:17:18 -0600

    added js function to append tip amount to DOM

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('test')
+var input = document.getElementsByClassName('form-control')[0];
+
+var button = document.getElementsByClassName('btn')[0];
+
+var output = document.getElementsByClassName('output')[0];
+
+function tip20(number){
+  var tip = (number/100)*20;
+  return tip;
+}
+
+button.addEventListener('click', function(){
+  output.innerHTML = 'You should tip $' + tip20(input.value).toFixed(2);
+});
